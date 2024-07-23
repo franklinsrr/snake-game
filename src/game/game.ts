a86a0a65a2ed902402b59712ab915554c900c2f8
franklin rodriguez <franklinserif@gmail.com>
2024-07-23 11:54:45 -0400

    refactor: :recycle: improve the main game system
    now is not just 1 dimension, is 2 dimesion now

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,12 +1,15 @@
 import { IBoard } from "@/interfaces/board";
+import { IState } from "@/interfaces/game";
 import { Board } from "@/board/board";
 import { Control } from "./control";
 import { Collision } from "./collision";
+import { Movement } from "./movement";
+
 
 export class Game {
     private Frames: number = 800;
     board: IBoard;
-    state: number = 0;
+    state: IState = { x: 0, y: 0 };
     direction: string = "";
 
     constructor(board: IBoard = new Board()) {
@@ -18,35 +21,31 @@ export class Game {
             throw new Error("there is not app id in the html file");
         }
 
+        const movement = new Movement();
+
         const stop = setInterval(() => {
             app.innerHTML = "";
             const controls = new Control();
             const collision = new Collision();
 
+            controls.controller(this.direction, this.state, (direction) => {
+                this.direction = direction;
+            });
+
             const isCollision = collision.detectCollision(this.state, this.direction);
 
             if (isCollision) {
+                console.log("collision detected");
                 clearInterval(stop);
-            }
-
-            if (this.direction === "up") {
-                this.state -= 44;
-            } else if (this.direction === "right") {
-                this.state += 1;
-            } else if (this.direction === "left") {
-                this.state -= 1;
-            } else if (this.direction === "down") {
-                this.state += 44;
             } else {
-                this.direction = "right"
-                this.state += 0;
+
+                const [newState, newDirection] = movement.init(this.state, this.direction);
+                this.direction = newDirection;
+                this.state = newState;
+
             }
 
             console.log("state: ", this.state);
-
-            controls.controller(this.direction, (direction) => {
-                this.direction = direction;
-            });
 
             this.board.renderBoard(app, this.state);
 
