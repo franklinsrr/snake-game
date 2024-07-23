7e4809882e57c5435a7890b9c0ce0775ee2e45d6
franklin rodriguez <franklinserif@gmail.com>
2024-07-22 20:34:43 -0400

    feat: :sparkles: better implementetion of game class

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,11 +1,14 @@
-import { IBoard } from "@/interface/game";
+import { IBoard } from "@/interfaces/board";
 import { Board } from "@/board/board";
-
+import { Control } from "./control";
+import { Collision } from "./collision";
 
 export class Game {
-    private renderTime: number = 2000;
-    private test: number = 0
+    private Frames: number = 800;
     board: IBoard;
+    state: number = 0;
+    direction: string = "";
+
     constructor(board: IBoard = new Board()) {
         this.board = board;
     }
@@ -15,11 +18,38 @@ export class Game {
             throw new Error("there is not app id in the html file");
         }
 
-        setInterval(() => {
-            this.board.renderBoard();
-            app.innerHTML = `
-                <span style="color: white; font-size: 100px; display: flex; justify-content: center; align-items: center; height: 100vh; width: 100vw">this is just a test ${this.test += 1}</span>
-            `
-        }, this.renderTime)
+        const stop = setInterval(() => {
+            app.innerHTML = "";
+            const controls = new Control();
+            const collision = new Collision();
+
+            const isCollision = collision.detectCollision(this.state, this.direction);
+
+            if (isCollision) {
+                clearInterval(stop);
+            }
+
+            if (this.direction === "up") {
+                this.state -= 44;
+            } else if (this.direction === "right") {
+                this.state += 1;
+            } else if (this.direction === "left") {
+                this.state -= 1;
+            } else if (this.direction === "down") {
+                this.state += 44;
+            } else {
+                this.direction = "right"
+                this.state += 0;
+            }
+
+            console.log("state: ", this.state);
+
+            controls.controller(this.direction, (direction) => {
+                this.direction = direction;
+            });
+
+            this.board.renderBoard(app, this.state);
+
+        }, this.Frames)
     }
 }
