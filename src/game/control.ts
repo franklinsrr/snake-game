2d81b32d39a9b1cb44145a6318e1d45885d48f4e
franklin rodriguez <franklinserif@gmail.com>
2024-07-23 11:53:41 -0400

    refactor: :recycle: improve the control system

--- src/game/control.ts
+++ src/game/control.ts
@@ -1,19 +1,32 @@
-export class Control {
-    direction: string = "left";
+import { IState } from "@/interfaces/game";
 
-    controller(direction: string, callback: (direction: string) => void) {
+export class Control {
+    controller(direction: string, state: IState, callback: (direction: string, state: IState) => void) {
+
+        if (direction === "init") {
+            const newState = { x: 0, y: 0 };
+            callback("right", newState);
+        }
+
         document.addEventListener("keyup", (event) => {
             if (event.key === "w" && direction !== "up") {
-                callback("up");
+                const newState = { x: state.x, y: state.y - 44 };
+                callback("up", newState);
 
             } else if (event.key === "d" && direction !== "right") {
-                callback("right");
+                const newState = { x: state.x + 1, y: state.y };
+                callback("right", newState);
 
             } else if (event.key === "a" && direction !== "left") {
-                callback("left");
+                const newState = { x: state.x - 1, y: state.y };
+                callback("left", newState);
 
             } else if (event.key === "s" && direction !== "down") {
-                callback("down");
+                const newState = { x: state.x, y: state.y + 44 };
+                callback("down", newState);
+            } else {
+                const newState = { x: 0, y: 0 };
+                callback("right", newState);
             }
         });
 
