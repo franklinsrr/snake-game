aa06ff951f36ebe9c234abd77dd0ebc8023322cc
franklin rodriguez <franklinserif@gmail.com>
2024-07-22 20:34:25 -0400

    refactor: :recycle: better implementention of board

--- src/board/board.ts
+++ src/board/board.ts
@@ -1,9 +1,21 @@
-export class Board {
-    constructor() {
+import { IBoard } from "@/interfaces/board";
+import { Snake } from "@/snake/snake";
 
-    }
+export class Board implements IBoard {
 
-    renderBoard() {
-        console.log("render board...");
+    renderBoard(app: HTMLDivElement | null, state: number) {
+        let init = 0;
+
+        const width = app?.style.width as unknown as number || 880;;
+        const height = app?.style.height as unknown as number || 880;
+
+        const squares = (width * height) / (20 * 20);
+
+        while (init < squares) {
+            const snake = new Snake();
+            const squareElement = snake.renderSnake(state, init);
+            app?.appendChild(squareElement);
+            init++;
+        }
     }
 }
