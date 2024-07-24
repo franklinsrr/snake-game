4a657e34f7be4db8c593c3445a246d2a910cc1b1
franklin rodriguez <franklinserif@gmail.com>
2024-07-23 22:00:12 -0400

    refactor: :recycle: implement the new cords state

--- src/game/control.ts
+++ src/game/control.ts
@@ -1,7 +1,7 @@
-import { IState } from "@/interfaces/game";
+import { ICords } from "@/interfaces/game";
 
 export class Control {
-    controller(direction: string, state: IState, callback: (direction: string, state: IState) => void) {
+    controller(direction: string, statePosition: ICords, callback: (direction: string, cords: ICords) => void) {
 
         if (direction === "init") {
             const newState = { x: 0, y: 0 };
@@ -10,19 +10,19 @@ export class Control {
 
         document.addEventListener("keyup", (event) => {
             if (event.key === "w" && direction !== "up") {
-                const newState = { x: state.x, y: state.y - 44 };
+                const newState = { x: statePosition.x, y: statePosition.y - 44 };
                 callback("up", newState);
 
             } else if (event.key === "d" && direction !== "right") {
-                const newState = { x: state.x + 1, y: state.y };
+                const newState = { x: statePosition.x + 1, y: statePosition.y };
                 callback("right", newState);
 
             } else if (event.key === "a" && direction !== "left") {
-                const newState = { x: state.x - 1, y: state.y };
+                const newState = { x: statePosition.x - 1, y: statePosition.y };
                 callback("left", newState);
 
             } else if (event.key === "s" && direction !== "down") {
-                const newState = { x: state.x, y: state.y + 44 };
+                const newState = { x: statePosition.x, y: statePosition.y + 44 };
                 callback("down", newState);
             } else {
                 const newState = { x: 0, y: 0 };
