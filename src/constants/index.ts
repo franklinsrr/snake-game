07bc8bd010134a6678c8b2f31a1f8a77584eee2d
franklin rodriguez <franklinserif@gmail.com>
2024-07-24 02:38:18 -0400

    feat: :sparkles: add new constants

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+export const DIRECTIONS = {
+    LEFT: "LEFT",
+    RIGHT: "RIGHT",
+    UP: "UP",
+    DOWN: "DOWN",
+    INIT: "INIT",
+}
+
+export const KEYS = {
+    UP: "w",
+    DOWN: "s",
+    RIGHT: "d",
+    LEFT: "a"
+}
