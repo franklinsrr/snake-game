87f30b98c8c00505e3b4c901d0da480b33d1a449
franklin rodriguez <franklinserif@gmail.com>
2024-07-24 02:38:59 -0400

    refactor: :recycle: improve the Board class and interfac

--- src/interfaces/board.ts
+++ src/interfaces/board.ts
@@ -1,7 +1,8 @@
-import { IFoodState } from "./food";
-import { IState } from "./game";
+import {ICords, } from "./game";
+import {Food} from "@/food/food.ts";
 
 export interface IBoard {
-    renderBoard(app: HTMLDivElement | null, state: IState, food: IFoodState): void;
+    table: ICords[][];
+    renderBoard(app: HTMLDivElement | null, snakePosition: ICords, food: Food): void;
 
 }
