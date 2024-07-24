ce4aeb1199365471bde788b2da4ad8c9f959bde6
franklin rodriguez <franklinserif@gmail.com>
2024-07-23 21:59:47 -0400

    refactor: :recycle: implement the new cords state

--- src/game/collision.ts
+++ src/game/collision.ts
@@ -1,18 +1,17 @@
-import { IState } from "@/interfaces/game";
+import { ICords } from "@/interfaces/game";
 
 export class Collision {
     private isCollision = false;
-    private topSides: IState[] = Array.from({ length: 44 }, (_, i) => ({ x: (i + 1), y: 1 }));
-    private bottomSides: IState[] = Array.from({ length: 44 }, (_, i) => ({ x: (i + 1), y: 44 }));
-    private rightSides: IState[] = Array.from({ length: 44 }, (_, i) => ({ x: 44, y: i + 1 }));
-    private leftSides: IState[] = Array.from({ length: 44 }, (_, i) => ({ x: 1, y: i + 1 }));
+    private topSides: ICords[] = Array.from({ length: 44 }, (_, i) => ({ x: (i + 1), y: 1 }));
+    private bottomSides: ICords[] = Array.from({ length: 44 }, (_, i) => ({ x: (i + 1), y: 44 }));
+    private rightSides: ICords[] = Array.from({ length: 44 }, (_, i) => ({ x: 44, y: i + 1 }));
+    private leftSides: ICords[] = Array.from({ length: 44 }, (_, i) => ({ x: 1, y: i + 1 }));
 
-    validate(position: IState, sides: IState[]) {
-
+    validate(position: ICords, sides: ICords[]) {
         return !!sides.find((side) => side.x === position.x && side.y === position.y);
     }
 
-    detectCollision(position: IState, direction: string) {
+    detectCollision(position: ICords, direction: string) {
         console.log("direction: ", direction);
 
         if (this.validate(position, this.rightSides) && direction === "right") {
