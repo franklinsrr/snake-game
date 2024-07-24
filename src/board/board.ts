703e80d043944d2ddaa10d08b6e0227105154777
franklin rodriguez <franklinserif@gmail.com>
2024-07-24 02:36:06 -0400

    refactor: :recycle: improve the snake class

--- src/board/board.ts
+++ src/board/board.ts
@@ -1,21 +1,40 @@
-import { IBoard } from "@/interfaces/board";
-import { IFoodState } from "@/interfaces/food";
-import { ICords, IState } from "@/interfaces/game";
-import { Snake } from "@/snake/snake";
+import { ICords } from "@/interfaces/game";
+import {Square} from "@board/Square.ts";
+import {Food} from "@/food/food.ts";
+import {IBoard} from "@interfaces/board.ts";
 
 export class Board implements IBoard {
-    private table: ICords[][];
+    table: ICords[][];
+
 
     constructor() {
         this.table = Array.from({ length: 44 }, (_, y) => Array.from({ length: 44 }, (_, x) => ({ y: (y + 1), x: (x + 1) })));
     }
 
-    renderBoard(app: HTMLDivElement | null, state: IState, food: IFoodState) {
+    render(snakePosition: ICords, food: Food,  currentSquare: ICords) {
+        const square = new Square();
+        const foodPosition = food.getFood().currentStatePosition;
+        const foodAmount = food.getFood().amount;
+        let squareElement;
 
+        if (snakePosition.x == currentSquare.x && snakePosition.y === currentSquare.y) {
+            squareElement = square.renderSquare(1);
+        } else {
+            squareElement = square.renderSquare(0);
+        }
+
+        if (foodPosition.x === currentSquare.x && foodPosition.y === currentSquare.y && foodAmount > 0) {
+            squareElement = square.renderSquare(2);
+        }
+
+
+        return  squareElement;
+    }
+
+    renderBoard(app: HTMLDivElement | null, snakePosition: ICords, food: Food) {
         for (const positions of this.table) {
             positions.forEach((square) => {
-                const snake = new Snake();
-                const squareElement = snake.renderSnake(state, square, food);
+              const squareElement=   this.render(snakePosition, food, square )
                 app?.appendChild(squareElement);
             })
         }
--- src/interfaces/snake.ts
+++ src/interfaces/snake.ts
@@ -1,8 +1,16 @@
-import { IFoodState } from "./food";
-import { ICords, IState } from "./game";
+import { ICords } from "./game";
 import { ISquare } from "./square";
+import {TDirections} from "@interfaces/controls.ts";
 
 export interface ISnake {
-    square: ISquare;
-    renderSnake(IState: IState, position: ICords, food: IFoodState): HTMLDivElement;
+    readonly square: ISquare;
+    readonly direction: TDirections;
+    readonly snakeState: ICords;
+    readonly prevSnakeState: ICords;
+    getSnakePosition(): ICords;
+    getPrevSnakePosition() : ICords;
+    getDirection():TDirections;
+    setDirection(direction: TDirections): void;
+    move(cords: ICords, direction: TDirections): void;
+    reconciliation(): void;
 }
