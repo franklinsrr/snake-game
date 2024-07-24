1d3604d172471db0cbb6cd1edfddca9801d56798
franklin rodriguez <franklinserif@gmail.com>
2024-07-24 02:37:47 -0400

    refactor: :recycle: improve the Movement class implementation

--- src/game/movement.ts
+++ src/game/movement.ts
@@ -1,27 +1,33 @@
 import { ICords } from "@/interfaces/game";
+import { TDirections } from "@interfaces/controls.ts";
+import { DIRECTIONS } from "@/constants";
 
 export class Movement {
+    private direction: TDirections = "INIT" as TDirections;
     private steps: ICords = { y: 1, x: 1 };
 
-    init(statePosition: ICords, direction: string): [ICords, string] {
-        this.steps = statePosition;
+    init(statePosition: ICords, direction: TDirections) {
 
-        if (direction === "up") {
+        if (direction === DIRECTIONS.UP) {
             this.steps = { y: statePosition.y - 1, x: statePosition.x };
+            this.direction = DIRECTIONS.UP as TDirections
 
-        } else if (direction === "right") {
+        } else if (direction === DIRECTIONS.RIGHT) {
             this.steps = { y: statePosition.y, x: statePosition.x + 1 };
+            this.direction = DIRECTIONS.RIGHT as TDirections
 
-        } else if (direction === "left") {
+        } else if (direction === DIRECTIONS.LEFT) {
             this.steps = { y: statePosition.y, x: statePosition.x - 1 };
+            this.direction = DIRECTIONS.LEFT as TDirections
 
-        } else if (direction === "down") {
+        } else if (direction === DIRECTIONS.DOWN) {
             this.steps = { y: statePosition.y + 1, x: statePosition.x };
+            this.direction = DIRECTIONS.DOWN as TDirections
         } else {
-            direction = "right"
+            this.direction = DIRECTIONS.RIGHT as TDirections
             this.steps = { x: 1, y: 1 };
         }
 
-        return [this.steps, direction];
+        return { move: this.steps, newDirection: this.direction as TDirections };
     }
 }
