8ee39b5bda1f6560011bae2dfa44087efeaf96a4
franklin rodriguez <franklinserif@gmail.com>
2024-07-23 22:00:24 -0400

    refactor: :recycle: implement the new snake state

--- src/snake/snake.ts
+++ src/snake/snake.ts
@@ -1,24 +1,32 @@
 import { Square } from "@/board/Square";
-import { IState } from "@/interfaces/game";
+import { IFoodState } from "@/interfaces/food";
+import { ICords, IState } from "@/interfaces/game";
 import { ISnake } from "@/interfaces/snake";
 import { ISquare } from "@/interfaces/square";
 
 export class Snake implements ISnake {
-    square: Square;
+    square: ISquare;
 
     constructor(square: ISquare = new Square()) {
         this.square = square;
     }
 
-    renderSnake(state: IState, currentSquare: IState) {
+    renderSnake(state: IState, currentSquare: ICords, food: IFoodState) {
 
         let squareElement;
+        const { currentStatePosition: StatePosition } = state;
+        const { currentStatePosition: FoodPosition } = food;
 
-        if (state.x == currentSquare.x && state.y === currentSquare.y) {
+        if (StatePosition.x == currentSquare.x && StatePosition.y === currentSquare.y) {
             squareElement = this.square.renderSquare(1);
 
         } else {
             squareElement = this.square.renderSquare(0);
+        }
+
+        if (FoodPosition.x === currentSquare.x && FoodPosition.y === currentSquare.y) {
+            squareElement = this.square.renderSquare(2);
+
         }
 
         return squareElement;
