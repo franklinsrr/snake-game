95bdc4f65077bee8729e52e0951fcd37ff7af1b9
franklin rodriguez <franklinserif@gmail.com>
2024-07-24 02:38:34 -0400

    refactor: :recycle: improve the Food class implementation

--- src/food/food.ts
+++ src/food/food.ts
@@ -4,14 +4,23 @@ export class Food {
     food: IFoodState;
 
     constructor() {
-        this.food = { currentStatePosition: { x: this.getRandom(), y: this.getRandom() }, amount: 1 };
+        this.food = { currentStatePosition: { x: this.getRandom(), y: this.getRandom() }, amount: 3 };
     }
 
     getRandom() {
         return Math.floor(Math.random() * 44) + 1;
     }
 
+    getNewFood() {
+        this.food.currentStatePosition = { x: this.getRandom(), y: this.getRandom() };
+        return this.food;
+    }
+
     getFood() {
         return this.food;
+    }
+
+    lessFood() {
+        this.food.amount -= 1;
     }
 }
