e92a7361b3780392dc1425925365fdea3c0a862f
franklin rodriguez <franklinserif@gmail.com>
2024-07-23 21:59:33 -0400

    refactor: :recycle: improve the Board class to adapt the new states

--- src/board/board.ts
+++ src/board/board.ts
@@ -1,20 +1,21 @@
 import { IBoard } from "@/interfaces/board";
-import { IState } from "@/interfaces/game";
+import { IFoodState } from "@/interfaces/food";
+import { ICords, IState } from "@/interfaces/game";
 import { Snake } from "@/snake/snake";
 
 export class Board implements IBoard {
-    private table: IState[][];
+    private table: ICords[][];
 
     constructor() {
         this.table = Array.from({ length: 44 }, (_, y) => Array.from({ length: 44 }, (_, x) => ({ y: (y + 1), x: (x + 1) })));
     }
 
-    renderBoard(app: HTMLDivElement | null, state: IState) {
+    renderBoard(app: HTMLDivElement | null, state: IState, food: IFoodState) {
 
         for (const positions of this.table) {
             positions.forEach((square) => {
                 const snake = new Snake();
-                const squareElement = snake.renderSnake(state, square);
+                const squareElement = snake.renderSnake(state, square, food);
                 app?.appendChild(squareElement);
             })
         }
