7fa24a44cf6f6268859a55d54a93224acb71f982
franklin rodriguez <franklinserif@gmail.com>
2024-07-23 21:59:00 -0400

    refactor: :recycle: implement the new global state

--- src/game/game.ts
+++ src/game/game.ts
@@ -4,13 +4,31 @@ import { Board } from "@/board/board";
 import { Control } from "./control";
 import { Collision } from "./collision";
 import { Movement } from "./movement";
+import { Food } from "@/food/food";
 
 
 export class Game {
-    private Frames: number = 800;
-    board: IBoard;
-    state: IState = { x: 0, y: 0 };
+    private Frames: number = 150;
+    private board: IBoard;
     direction: string = "";
+    private state: IState = {
+        currentStatePosition: { x: 0, y: 0 },
+        food: {
+            currentStatePosition: {
+                x: 0, y: 0
+            },
+            amount: 0
+        }
+    };
+    private prevState: IState = {
+        currentStatePosition: { x: 0, y: 0 },
+        food: {
+            currentStatePosition: {
+                x: 0, y: 0
+            },
+            amount: 0
+        }
+    };
 
     constructor(board: IBoard = new Board()) {
         this.board = board;
@@ -22,33 +40,31 @@ export class Game {
         }
 
         const movement = new Movement();
+        const food = new Food();
 
         const stop = setInterval(() => {
             app.innerHTML = "";
             const controls = new Control();
             const collision = new Collision();
 
-            controls.controller(this.direction, this.state, (direction) => {
+            controls.controller(this.direction, this.state.currentStatePosition, (direction) => {
                 this.direction = direction;
             });
 
-            const isCollision = collision.detectCollision(this.state, this.direction);
+            const isCollision = collision.detectCollision(this.state.currentStatePosition, this.direction);
 
             if (isCollision) {
                 console.log("collision detected");
+                this.board.renderBoard(app, this.prevState, food.getFood());
                 clearInterval(stop);
             } else {
-
-                const [newState, newDirection] = movement.init(this.state, this.direction);
+                const [newState, newDirection] = movement.init(this.state.currentStatePosition, this.direction);
                 this.direction = newDirection;
-                this.state = newState;
-
+                this.state.currentStatePosition = newState;
+                this.board.renderBoard(app, this.state, food.getFood());
             }
-
             console.log("state: ", this.state);
-
-            this.board.renderBoard(app, this.state);
-
+            this.prevState = this.state;
         }, this.Frames)
     }
 }
