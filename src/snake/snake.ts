e1bb3e5459edb4ed508b55a6d35a39ffffe9604e
franklin rodriguez <franklinserif@gmail.com>
2024-07-24 21:43:51 -0400

    refactor: :recycle: new snake body classes

--- src/snake/snake.ts
+++ src/snake/snake.ts
@@ -3,18 +3,33 @@ import { ICords } from "@/interfaces/game";
 import { ISnake } from "@/interfaces/snake";
 import { ISquare } from "@/interfaces/square";
 import { TDirections } from "@interfaces/controls.ts";
+import { Body } from "./body";
 
 export class Snake implements ISnake {
     square: ISquare;
     direction: TDirections;
     snakeState: ICords;
     prevSnakeState: ICords;
+    body;
 
     constructor(square: ISquare = new Square()) {
         this.square = square;
         this.direction = "INIT";
         this.snakeState = { x: 1, y: 1 };
         this.prevSnakeState = { x: 1, y: 1 };
+        this.body = new Body();
+        this.body.insertNode({ cords: { x: 1, y: 1 }, direction: "RIGHT" })
+        this.body.insertNode({ cords: { x: 2, y: 1 }, direction: "RIGHT" })
+        this.body.insertNode({ cords: { x: 3, y: 1 }, direction: "RIGHT" })
+        this.body.insertNode({ cords: { x: 4, y: 1 }, direction: "RIGHT" })
+        this.body.insertNode({ cords: { x: 5, y: 1 }, direction: "RIGHT" })
+        this.body.insertNode({ cords: { x: 6, y: 1 }, direction: "RIGHT" })
+        this.body.insertNode({ cords: { x: 7, y: 1 }, direction: "RIGHT" })
+        this.body.insertNode({ cords: { x: 8, y: 1 }, direction: "RIGHT" })
+        this.body.insertNode({ cords: { x: 9, y: 1 }, direction: "RIGHT" })
+        this.body.insertNode({ cords: { x: 10, y: 1 }, direction: "RIGHT" })
+        this.body.insertNode({ cords: { x: 11, y: 1 }, direction: "RIGHT" })
+        this.body.insertNode({ cords: { x: 12, y: 1 }, direction: "RIGHT" })
     }
 
     getSnakePosition(): ICords {
@@ -36,10 +51,15 @@ export class Snake implements ISnake {
     move(cords: ICords, direction: TDirections) {
         this.snakeState = cords;
         this.direction = direction;
+        this.body.moveNodes({ cords, direction });
     }
 
     reconciliation() {
         this.prevSnakeState = this.snakeState;
+    }
+
+    getSnakeBody() {
+        return this.body.getNodesList();
     }
 
 }
