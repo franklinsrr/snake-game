529a06457678b408416d243394d45ef9c1cbed3d
franklin rodriguez <franklinserif@gmail.com>
2024-07-24 21:43:30 -0400

    feat: :sparkles: new snake body classes

--- src/interfaces/snake.ts
+++ src/interfaces/snake.ts
@@ -1,16 +1,27 @@
 import { ICords } from "./game";
 import { ISquare } from "./square";
-import {TDirections} from "@interfaces/controls.ts";
+import { TDirections } from "@interfaces/controls.ts";
 
 export interface ISnake {
     readonly square: ISquare;
     readonly direction: TDirections;
     readonly snakeState: ICords;
     readonly prevSnakeState: ICords;
+
     getSnakePosition(): ICords;
-    getPrevSnakePosition() : ICords;
-    getDirection():TDirections;
+    getPrevSnakePosition(): ICords;
+    getDirection(): TDirections;
     setDirection(direction: TDirections): void;
     move(cords: ICords, direction: TDirections): void;
     reconciliation(): void;
+}
+
+export interface IData {
+    cords: ICords,
+    direction: TDirections;
+}
+
+export interface INode {
+    data: IData;
+    next: INode | null;
 }
